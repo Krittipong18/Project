2b425bb837621f9b94e3a845933b4abf527326b8
Krittipong18 <kkrittipong18@gmail.com>
2024-12-21 16:42:03 +0700

    Delete faq.js

--- faq.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const faqItems = document.querySelectorAll(".faq-item");
-    faqItems.forEach((item) => {
-        item.querySelector(".faq-question").addEventListener("click", () => {
-            item.classList.toggle("active");
-        });
-    });
-});
